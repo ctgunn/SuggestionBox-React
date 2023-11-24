a8b4f1b936807c931af786d769d9237593bd331c
Cliff Gunn <cliff.gunn@gmail.com>
2023-11-24 01:02:18 -0600

    Made one small tweak to keep things consistent.

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -8,13 +8,13 @@ type ProtectedProps = {
 }
 
 const Authenticated = ({element}: ProtectedProps): React.JSX.Element => {
-    // useUserState();
+    // useAuthentication();
 
     return element;
 };
 
-const protectedRoute = (route: string, element: React.JSX.Element) => {
-    <Route key={route} path={route} element={<Authenticated element={element} />} />
+const protectedRoute = (route: string, element: React.JSX.Element): React.JSX.Element => {
+    return <Route key={route} path={route} element={<Authenticated element={element} />} />
 };
 
 const publicRoute = (route: string, element: React.JSX.Element): React.JSX.Element => {
